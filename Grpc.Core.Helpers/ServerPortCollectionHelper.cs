3a1559fae9992f32f3e2cc64939a2df431260f49
Artyom S <artyom@atftech.com>
2019-02-10 10:01:10 +0200

    Initial commit

---
+++ Grpc.Core.Helpers/ServerPortCollectionHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Helpers;
+using System.Net.NetworkInformation;
+
+namespace Grpc.Core.Helpers
+{
+    public static class ServerPortCollectionHelper
+    {
+        public static IEnumerable<EndPoint> ToEndpoints(this ServerPort port)
+        {
+            var endpoints = new List<EndPoint>();
+
+            if (port.Host == IPAddress.Any.ToString())
+            {
+                var ipAddresses = NetworkInterface.GetAllNetworkInterfaces().GetIpAddresses();
+
+                endpoints.AddRange(ipAddresses.Select(address => new DnsEndPoint(address.ToString(), port.BoundPort)));
+            }
+            else if (port.Host == IPAddress.IPv6Any.ToString())
+            {
+                var ipAddresses = NetworkInterface.GetAllNetworkInterfaces().GetIpAddresses();
+
+                endpoints.AddRange(ipAddresses.Select(address => new DnsEndPoint(address.ToString(), port.BoundPort)));
+            }
+            else
+            {
+                endpoints.Add(new DnsEndPoint(port.Host, port.BoundPort));
+            }
+
+            return endpoints;
+        }
+
+        public static IEnumerable<EndPoint> ToEndpoints(this Server.ServerPortCollection collection)
+        {
+            return collection.SelectMany(p => p.ToEndpoints());
+        }
+    }
+}
